8f9f217f762f90c87027116221b7b6803d5f55c1
Tarun Lohani <t.lohani@gmail.com>
2017-12-10 03:37:21 -0500

    Changes

--- id3decisiontree.py
+++ id3decisiontree.py
@@ -2,7 +2,13 @@ from __future__ import division
 import scipy.stats as st
 import numpy as np
 import math
+import collections
 # DO NOT CHANGE THIS CLASS
+
+
+def filterArray(arr, val):
+    return [ t for t in arr if t != val]
+
 class TreeNode():
     def __init__(self, data='T',children=[-1]*5):
         self.nodes = list(children)
@@ -11,10 +17,13 @@ class TreeNode():
 
 
 
-    def entropy(self, index):
+    def entropy(self, index, data = None):
         hashmap = dict()
+        if data is None:
+            column = [row[index] for row in train_features]
+        else:
+            column = data
 
-        column = [row[index] for row in train_features]
         for val in column:
             hashmap[val] = hashmap.get(val, 0) + 1
         freqs = np.array(hashmap.values())
@@ -25,8 +34,48 @@ class TreeNode():
                 retVal += -(freq*np.log(freq))
         return retVal
 
+    def infoGain(self, index, data=None):
+        if data is None:
+            column = [row[index] for row in train_features ]
+        else:
+            column = data
+
+        freq_map = collections.Counter(column)
+        #print freq_map
+        count = len(train_labels)
+
+        retVal = 0.0
+
+        for k in freq_map.keys():
+
+
+
+            #### need to filter something here.
+            # filtered
+            ent = self.entropy(-1, data = filtered)
+            print " entropy is " + str(ent)
+            retVal += freq_map[k] * ent
+
+        t =  labelEntropy - retVal/count
+        #print "info gain is " + str(t)
+        return t
+
+    def pickBestAttr(self):
+        bestGain = float('-inf')
+        bestFeature = None
+        bestIdx = 0
+        #print feature_name
+        for count, feature in enumerate(feature_name):
+            currentGain = self.infoGain(count, data = None)
+            #print currentGain
+            if currentGain > bestGain:
+                bestGain = currentGain
+                bestFeature = feature
+                bestIdx = count
+        return bestIdx, bestFeature
+
     def buildDecisionTree(self):
-        print ""
+        print self.pickBestAttr()
 
 # def main:
 #     pass
@@ -34,6 +83,7 @@ class TreeNode():
 if __name__ == '__main__':
     train_features = []
     train_labels = []
+    feature_name = []
 
     train_feat_file = open('trainfeat.csv')
     for line in train_feat_file:
@@ -46,4 +96,14 @@ if __name__ == '__main__':
     for line in train_label_file:
         train_labels.append(int(line))
     #print(train_labels)
-    print entropy(220)
+
+    feature_name_file = open('featnames.csv')
+    for line in feature_name_file:
+        feature_name.append(line)
+
+    root = TreeNode()
+    #print root.entropy(270)
+
+    labelEntropy = root.entropy(-1, data=train_labels)
+    #print train_features
+    root.buildDecisionTree()
